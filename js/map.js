abaff935874afb354f08c892e67b6027bc6bdfc2
Andriy Babayev <77857802+AndreyBabayev@users.noreply.github.com>
2022-08-27 20:33:47 +0300

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
 function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var myCenter = new google.maps.LatLng(45.579591,9.164906); 
-  var mapOptions = {center: myCenter, zoom: 13};
-  var map = new google.maps.Map(mapCanvas,mapOptions);
-  var marker = new google.maps.Marker({
+  let mapCanvas = document.getElementById("map");
+  let myCenter = new google.maps.LatLng(49.946002, 36.308349, 17);
+  let mapOptions = { center: myCenter, zoom: 13 };
+  let map = new google.maps.Map(mapCanvas, mapOptions);
+  let marker = new google.maps.Marker({
     position: myCenter,
   });
   marker.setMap(map);
